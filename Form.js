d414bd24a0d82a32107456e335d0b2d8cc63ffbe
Karan_Chaudhary <kdchy12515@gmail.com>
2025-06-09 21:22:35 +0545

    General Form Design

---
+++ Form.js
@@ -0,0 +1,5 @@
+// Example: Alert on submit
+document.querySelector('form').addEventListener('submit', function(e) {
+    alert('Form submitted!');
+    // e.preventDefault(); // Uncomment to prevent actual submission
+});
